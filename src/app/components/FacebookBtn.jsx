8b2e3e7d7131f8ddade6502092731ad209da4e22
Azza Sallam <Azzasallam369@gmail.com>
2025-11-24 14:54:32 +0200

    edit position in switch language for contact buttons

--- src/app/components/FacebookBtn.jsx
+++ src/app/components/FacebookBtn.jsx
@@ -1,10 +1,14 @@
 'use client';
 import Image from 'next/image';
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const FacebookBtn = () => {
 
-    // Function to handle WhatsApp redirection
+    const { i18n } = useTranslation();
+    const isRTL = i18n.language === 'ar';
+
+    // Function to handle Facebook redirection
     function goToFacebook() {
         window.open("https://www.facebook.com/profile.php?id=61568360861145", "_blank");
     }
@@ -13,9 +17,9 @@ const FacebookBtn = () => {
     return (
         <button
             onClick={goToFacebook}
-            className="fixed bottom-28 right-4 w-[76px] h-[76px] rounded-full flex items-center justify-center bg-[#EBE8E033] z-30"
+            className={`fixed bottom-28 w-[76px] h-[76px] rounded-full flex items-center justify-center bg-[#EBE8E033] z-30 ${isRTL ? 'right-4' : 'left-4'}`}
             >
-                <Image src='/assets/icons/FaceLogo.png' alt='WhatsApp' width={100} height={100}  className='w-[65%] h-[65%]'/>
+                <Image src='/assets/icons/FaceLogo.png' alt='Facebook' width={100} height={100}  className='w-[65%] h-[65%]'/>
         </button>
       )
 }
--- src/app/components/WhatsAppBtn.jsx
+++ src/app/components/WhatsAppBtn.jsx
@@ -1,8 +1,12 @@
 'use client';
 import Image from 'next/image';
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const WhatsAppBtn = () => {
+
+    const { i18n } = useTranslation();
+    const isRTL = i18n.language === 'ar';
 
     // Function to handle WhatsApp redirection
     function sendToWhatsApp() {
@@ -13,9 +17,9 @@ const WhatsAppBtn = () => {
     return (
         <button
             onClick={sendToWhatsApp}
-            className="fixed bottom-8 right-4 w-[76px] h-[76px] rounded-full flex items-center justify-center bg-[#EBE8E033] z-30"
+            className={`fixed bottom-8 w-[76px] h-[76px] rounded-full flex items-center justify-center bg-[#EBE8E033] z-30 ${isRTL ? 'right-4' : 'left-4'}`}
             >
-                <span className='absolute w-3 h-3 rounded-full bg-[#F52338] top-4 right-5'></span>
+                <span className={`absolute w-3 h-3 rounded-full bg-[#F52338] top-4 ${isRTL ? 'left-5' : 'right-5'}`}></span>
                 <Image src='/assets/icons/whatsapp.png' alt='WhatsApp' width={100} height={100}  className='w-[65%] h-[65%]'/>
         </button>
       )
